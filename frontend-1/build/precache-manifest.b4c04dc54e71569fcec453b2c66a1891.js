51ce36b1db4f825a6cde3013a8a8709484580451
Priya Tiru <priyatiru522@gmail.com>
2021-06-02 17:32:27 +0530

    748r

---
+++ frontend-1/build/precache-manifest.b4c04dc54e71569fcec453b2c66a1891.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f76168ada314e2592b5485f8a6e7a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5aa2ece49820e66f7b6",
+    "url": "/static/js/2.16eb1ecc.chunk.js"
+  },
+  {
+    "revision": "0ac927c44a4a91d22374",
+    "url": "/static/js/main.23a5161e.chunk.js"
+  },
+  {
+    "revision": "1078bbc7f8c94a05ce0d",
+    "url": "/static/js/runtime-main.1aa32bfa.js"
+  }
+]);
